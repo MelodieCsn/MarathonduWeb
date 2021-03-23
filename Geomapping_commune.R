9caf8a9201adca8c435a825297ad700faa884ecc
jo_miashs <d5cd0a8f-453d-45a7-b5ed-a8ac232537bc@anonaddy.me>
2021-03-23 13:39:11 +0100

    Récupération long et lat en fonction : Nom de ville, sinon code postal, sinon code insee

--- Geomapping_commune.R
+++ Geomapping_commune.R
@@ -1,35 +1,27 @@
-
 library(rgeoapi)
 library(openxlsx)
 
 
 liste_participants <- read.xlsx("R_Data/LISTE PARTICIPANTS OBSERVATOIRE.xlsx", rowNames = TRUE)
 
-Unaccent <- function(text) {
-  text <- gsub("['`^~\"]", " ", text)
-  text <- iconv(text, to="ASCII")
-  text <- gsub("['`^~\"]", "", text)
-  return(text)
+liste_communes <- subset(liste_participants, select = c("CODE.POSTAL", "COMMUNE"))
+
+get_commune_data <- function(row) {
+  results <- tryCatch({
+    ComByName(row$COMMUNE)
+  }, warning = function(){
+    tryCatch({
+      ComByPostal(row$CODE.POSTAL)
+    }, warning = function(){
+      ComByCode(row$CODE.POSTAL)
+    })
+  })
+
+  return(results[1,])
 }
 
-liste_participants$COMMUNE = lapply(liste_participants$COMMUNE, Unaccent)
-
-
-getLat <- function(code) {
-  df = ComByName(code)
-  a = df[1,'lat']
-    return(a)
+liste_communes
+for (i in seq_len(nrow(liste_communes))) {
+  liste_communes[i, "lat"] <- get_commune_data(liste_communes[i,])$lat
+  liste_communes[i, "long"] <- get_commune_data(liste_communes[i,])$long
 }
-
-getLong <- function(code) {
-  df = ComByName(code)
-  a = df[1,'long']
-  return(a)
-}
-
-
-liste_participants$lat = as.numeric(lapply(liste_participants$COMMUNE, getLat))
-
-liste_participants$long = as.numeric(lapply(liste_participants$COMMUNE, getLong))
-
-
